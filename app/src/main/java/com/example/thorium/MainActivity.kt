600707ae8050ce7e263a0e5627547ac2416ce041
Mobin Dariush <mobindariush@outlook.com>
2021-06-18 18:17:08 +0430

    Add mapbox map in MainActivity

--- app/src/main/java/com/example/thorium/MainActivity.kt
+++ app/src/main/java/com/example/thorium/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.thorium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.thorium.databinding.ActivityMainBinding
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.maps.Style
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token))
+
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+        with (binding.mapView) {
+            onCreate(savedInstanceState)
+            getMapAsync { mapboxMap ->
+                mapboxMap.setStyle(Style.MAPBOX_STREETS)
+            }
+        }
     }
 }
