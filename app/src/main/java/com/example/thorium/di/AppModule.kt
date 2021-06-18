da93c0462baeec9a9fced35c2d7c808385c94689
Mobin Dariush <mobindariush@outlook.com>
2021-06-18 16:52:27 +0430

    Add ThoriumApp

---
+++ app/src/main/java/com/example/thorium/di/AppModule.kt
@@ -0,0 +1,9 @@
+package com.example.thorium.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule
