f96a0524707c03817d88f0f0e78044b9faa989ab
Mobin Dariush <mobindariush@outlook.com>
2021-06-20 20:01:57 +0430

    Create separate classes for different cells

--- app/src/main/java/com/example/thorium/gsm/CellularService.kt
+++ app/src/main/java/com/example/thorium/gsm/CellularService.kt
@@ -3,6 +3,7 @@ package com.example.thorium.gsm
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.telephony.CellInfo
 import android.telephony.TelephonyManager
 import android.util.Log
@@ -15,9 +16,11 @@ import android.telephony.CellInfoLte
 
 import android.telephony.CellInfoWcdma
 
-import android.telephony.CellSignalStrength
-
 import androidx.core.content.ContextCompat
+import com.example.common.entity.CellGsm
+import com.example.common.entity.CellLte
+import com.example.common.entity.CellWcdma
+import java.lang.IllegalArgumentException
 
 
 class CellularService {
@@ -65,50 +68,51 @@ class CellularService {
     }
 
     private fun parseCellInfo(cellInfo: CellInfo): Cell {
-        var mcc = 0
-        var mnc = 0
-        var lac = 0
-        var cid = 0
-        var registered = false
-        var radio = ""
-        var cellSignalStrength: CellSignalStrength? = null
         when (cellInfo) {
             is CellInfoWcdma -> {
-                mcc = cellInfo.cellIdentity.mcc
-                mnc = cellInfo.cellIdentity.mnc
-                lac = cellInfo.cellIdentity.lac
-                cid = cellInfo.cellIdentity.cid
-                registered = cellInfo.isRegistered
-                cellSignalStrength = cellInfo.cellSignalStrength
-                radio = "wcdma"
+                return CellWcdma(
+                    mcc = cellInfo.cellIdentity.mcc,
+                    mnc = cellInfo.cellIdentity.mnc,
+                    lac = cellInfo.cellIdentity.lac,
+                    psc = cellInfo.cellIdentity.psc,
+                    registered = cellInfo.isRegistered,
+                    strength = cellInfo.cellSignalStrength.dbm
+                )
             }
             is CellInfoLte -> {
-                mcc = cellInfo.cellIdentity.mcc
-                mnc = cellInfo.cellIdentity.mnc
-                lac = cellInfo.cellIdentity.tac
-                cid = cellInfo.cellIdentity.ci
-                registered = cellInfo.isRegistered
-                cellSignalStrength = cellInfo.cellSignalStrength
-                radio = "lte"
+                return CellLte(
+                    mcc = cellInfo.cellIdentity.mcc,
+                    mnc = cellInfo.cellIdentity.mnc,
+                    tac = cellInfo.cellIdentity.tac,
+                    pci = cellInfo.cellIdentity.pci,
+                    registered = cellInfo.isRegistered,
+                    strength = cellInfo.cellSignalStrength.dbm
+                )
             }
             is CellInfoGsm -> {
-                mcc = cellInfo.cellIdentity.mcc
-                mnc = cellInfo.cellIdentity.mnc
-                lac = cellInfo.cellIdentity.lac
-                cid = cellInfo.cellIdentity.cid
-                registered = cellInfo.isRegistered
-                cellSignalStrength = cellInfo.cellSignalStrength
-                radio = "gsm"
+                return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                    CellGsm(
+                        mcc = cellInfo.cellIdentity.mcc,
+                        mnc = cellInfo.cellIdentity.mnc,
+                        lac = cellInfo.cellIdentity.lac,
+                        bsic = cellInfo.cellIdentity.bsic,
+                        registered = cellInfo.isRegistered,
+                        strength = cellInfo.cellSignalStrength.dbm
+                    )
+                } else {
+                    CellGsm(
+                        mcc = cellInfo.cellIdentity.mcc,
+                        mnc = cellInfo.cellIdentity.mnc,
+                        lac = cellInfo.cellIdentity.lac,
+                        bsic = cellInfo.cellIdentity.psc, //bsic requires android 7+
+                        registered = cellInfo.isRegistered,
+                        strength = cellInfo.cellSignalStrength.dbm
+                    )
+                }
+            }
+            else -> {
+                throw IllegalArgumentException("Cell info type not handled")
             }
         }
-        return Cell(
-            mcc = mcc,
-            mnc = mnc,
-            lac = lac,
-            cid = cid,
-            radio = radio,
-            strength = cellSignalStrength?.dbm ?: 0,
-            registered = registered
-        )
     }
 }
--- common/src/main/java/com/example/common/entity/Cell.kt
+++ common/src/main/java/com/example/common/entity/Cell.kt
@@ -1,23 +1,33 @@
 package com.example.common.entity
 
-data class Cell(
-    val mcc: Int,
-    val mnc: Int,
+sealed class Cell(
+    open val mcc: Int,
+    open val mnc: Int,
+    open val strength: Int,
+    open val registered: Boolean
+)
+
+data class CellWcdma(
+    override val mcc: Int,
+    override val mnc: Int,
     val lac: Int,
-    val cid: Int,
-    val strength: Int,
-    val radio: String,
-    val registered: Boolean
+    val psc: Int,
+    override val strength: Int,
+    override val registered: Boolean
+) : Cell(
+    mcc = mcc,
+    mnc = mnc,
+    strength = strength,
+    registered = registered
 ) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (other !is Cell) return false
+        if (other !is CellWcdma) return false
 
         if (mcc != other.mcc) return false
         if (mnc != other.mnc) return false
         if (lac != other.lac) return false
-        if (cid != other.cid) return false
-        if (radio != other.radio) return false
+        if (psc != other.psc) return false
         return true
     }
 
@@ -25,8 +35,73 @@ data class Cell(
         var result = mcc
         result = 31 * result + mnc
         result = 31 * result + lac
-        result = 31 * result + cid
-        result = 31 * result + radio.hashCode()
+        result = 31 * result + psc
+        return result
+    }
+}
+
+data class CellLte(
+    override val mcc: Int,
+    override val mnc: Int,
+    val tac: Int,
+    val pci: Int,
+    override val strength: Int,
+    override val registered: Boolean
+) : Cell(
+    mcc = mcc,
+    mnc = mnc,
+    strength = strength,
+    registered = registered
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CellLte) return false
+
+        if (mcc != other.mcc) return false
+        if (mnc != other.mnc) return false
+        if (tac != other.tac) return false
+        if (pci != other.pci) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = mcc
+        result = 31 * result + mnc
+        result = 31 * result + tac
+        result = 31 * result + pci
+        return result
+    }
+}
+
+data class CellGsm(
+    override val mcc: Int,
+    override val mnc: Int,
+    val lac: Int,
+    val bsic: Int,
+    override val strength: Int,
+    override val registered: Boolean
+) : Cell(
+    mcc = mcc,
+    mnc = mnc,
+    strength = strength,
+    registered = registered
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is CellGsm) return false
+
+        if (mcc != other.mcc) return false
+        if (mnc != other.mnc) return false
+        if (lac != other.lac) return false
+        if (bsic != other.bsic) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = mcc
+        result = 31 * result + mnc
+        result = 31 * result + lac
+        result = 31 * result + bsic
         return result
     }
 }
