99f9264013b6b4bceced1b22d2bd2d01e2554461
Mobin Dariush <mobindariush@outlook.com>
2021-06-27 15:04:34 +0430

    Fix timer bug

--- app/src/main/java/com/example/thorium/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/thorium/ui/main/MainViewModel.kt
@@ -1,16 +1,19 @@
 package com.example.thorium.ui.main
 
 import androidx.annotation.IdRes
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.viewModelScope
 import com.example.common.entity.AppState
 import com.example.thorium.R
 import com.example.usecase.interactor.ChangeAppStateUseCase
 import com.example.usecase.repository.AppStateRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
@@ -21,6 +24,8 @@ class MainViewModel @Inject constructor(
     val navigationAction = appStateRepository.navigationAction.asLiveData()
     val appState = appStateRepository.appState.asLiveData()
 
+    private val _timer = MutableLiveData<Int>()
+    val timer: LiveData<Int> = _timer
 
     fun onStart() {
         viewModelScope.launch {
@@ -42,6 +47,10 @@ class MainViewModel @Inject constructor(
                 }
             }
         }
+    }
+
+    fun onTimerChanged(time: Int) {
+        _timer.value = time
     }
 
 }
--- app/src/main/java/com/example/thorium/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/thorium/ui/settings/SettingsFragment.kt
@@ -4,21 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.ArrayRes
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.common.entity.Preference
+import com.example.thorium.R
 import com.example.thorium.databinding.FragmentSettingsBinding
+import com.example.thorium.ui.main.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import java.lang.IllegalArgumentException
+import kotlinx.android.synthetic.main.fragment_settings.npAutoLogTimer
 import kotlinx.android.synthetic.main.fragment_settings.rvList
 
 
 @AndroidEntryPoint
 class SettingsFragment : Fragment(), PreferenceAdapter.OnPreferenceChange {
 
+    private lateinit var timeToIndex: Map<Int, Int>
+    private lateinit var indexToTime: Map<Int, Int>
+
     private var adapter: PreferenceAdapter? = null
     private var _binding: FragmentSettingsBinding? = null
     private val settingsViewModel: SettingsViewModel by viewModels()
+    private val parentViewModel: MainViewModel by activityViewModels()
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -32,7 +42,6 @@ class SettingsFragment : Fragment(), PreferenceAdapter.OnPreferenceChange {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         init()
         observe()
     }
@@ -41,6 +50,10 @@ class SettingsFragment : Fragment(), PreferenceAdapter.OnPreferenceChange {
         settingsViewModel.preferenceList.observe(viewLifecycleOwner, {
             adapter?.submitList(it)
         })
+
+        parentViewModel.timer.observe(viewLifecycleOwner, { time ->
+            binding.npAutoLogTimer.value = timeToIndex[time]!!
+        })
     }
 
     private fun init() {
@@ -48,6 +61,38 @@ class SettingsFragment : Fragment(), PreferenceAdapter.OnPreferenceChange {
 
         rvList.layoutManager = LinearLayoutManager(requireContext())
         rvList.adapter = adapter
+
+        setupNumberPicker(R.array.times)
+        npAutoLogTimer.setOnValueChangedListener { picker, oldVal, newVal ->
+            if (newVal != oldVal) {
+                parentViewModel.onTimerChanged(indexToTime[newVal]!!)
+            }
+        }
+    }
+
+    private fun setupNumberPicker(@ArrayRes config: Int) {
+        val displayedValues = resources.getStringArray(config)
+        npAutoLogTimer.displayedValues = displayedValues
+        npAutoLogTimer.minValue = 0
+        npAutoLogTimer.maxValue = displayedValues.size - 1
+
+        indexToTime = displayedValues.mapIndexed { index, s ->
+            val timeValue = when (s) {
+                getString(R.string.time_5s) -> 5
+                getString(R.string.time_10s) -> 10
+                getString(R.string.time_30s) -> 30
+                getString(R.string.time_1m) -> 60
+                getString(R.string.time_2m) -> 2 * 60
+                getString(R.string.time_5m) -> 5 * 60
+                getString(R.string.time_10m) -> 10 * 60
+                getString(R.string.time_30m) -> 30 * 60
+                getString(R.string.time_1h) -> 60 * 60
+                else -> throw IllegalArgumentException("Invalid time string in config $s")
+            }
+            index to timeValue
+        }.toMap()
+
+        timeToIndex = indexToTime.entries.associate { (k, v) -> v to k }
     }
 
     override fun onDestroyView() {
